ad5c0f8365f7f0c0f6c189b4cfbbde52f9c97c99
amomotov35-cmd <a.momotov35@gmail.com>
2025-10-03 15:33:33 +0300

    2

--- dz/dz2/massiv/nomer2.cpp
+++ dz/dz2/massiv/nomer2.cpp
@@ -1,23 +1,36 @@
 // #include <iostream>
-// #include <cmath>
 // using namespace std;
 // int main() {
 //     setlocale(LC_ALL, "rus");
 //     int n;
-//     int s = 0;
-//     cout << "Введите количество элементов массива (>=1): ";
+//     cout << "Введите количество элементов массива: ";
 //     cin >> n;
-//     int arr[n];
-//     cout << "Введите элементы массива:" << endl;        
+//     if (n <= 0) {
+//         cout << "Количество элементов должно быть натуральным числом." << endl;
+//         return 1;
+//     }
+//     int* arr = new int[n];
+//     cout << "Введите " << n << " целых чисел:" << endl; 
+//     int s = 0;
+//     int max = INT_MIN;
 //     for (int i = 0; i < n; i++) {
 //         cin >> arr[i];
-//     }
-//     for (int i = 0; i < n; i++) {
-//         if (arr[i]/2=0) {
-                
+//         if (arr[i] % 2 == 0) {
+//             s++;
 //         }
-//         else 
+//         else {
+//             if (arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
 //     }
-//     cout << "Количество элементов:" << " " << s << endl;
+//         delete[] arr;
+//         if (max == INT_MIN) {
+//             cout << "Нет нечетных чисел в последовательности." << endl;
+//         }
+//         else {
+//             cout << "Наибольшее нечетное число: " << max << endl;
+//         }
+//         cout << "Количество четных элементов:" << " " << s << endl;   
 //     return 0;
 // }
